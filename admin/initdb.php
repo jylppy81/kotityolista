d8db12b8f02bd79844d24191ca456b4044dcc1e0
Jyri Björkman <jyri.bjorkman@gmail.com>
2018-07-29 13:14:58 +0300

    Reorganising

--- admin/initdb.php
+++ admin/initdb.php
@@ -1,22 +1,21 @@
 <html>
 <head>
-<title>Database initialization</head>
+<title>Database initialization</title>
 <meta charset="UTF-8">
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 </head>
 <body>
 <?php
-
-include 'dbconnection.php';
+include '../dbconnection.php';
 
 #database is defined in dbconnection.php and must exist before running this file.
 
 if(isset($_POST["toiminto"])) { $toiminto = $_POST["toiminto"]; }
 
 if($toiminto == "initialize") {
-  $query = "DROP TABLE henkilo, kotityo, tapahtuma"
+  $query = "DROP TABLE henkilo, kotityo, tapahtuma";
 
-  $conn->query($query)
+  $conn->query($query);
 
   $query = "CREATE TABLE `henkilo` (
   `henkiloid` int(6) unsigned NOT NULL AUTO_INCREMENT,
@@ -33,7 +32,7 @@ if($toiminto == "initialize") {
   `nimi` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `toistuvuus` int(11) DEFAULT NULL,
   PRIMARY KEY (`kotityoid`)
-  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci"
+  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
 
   $conn->query($query);
 
@@ -43,7 +42,7 @@ if($toiminto == "initialize") {
   `kotityoid` int(6) NOT NULL,
   `pvm` datetime NOT NULL,
   PRIMARY KEY (`tapahtumaid`)
-  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci"
+  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
 
   $conn->query($query);
 ?>
@@ -54,8 +53,11 @@ if($toiminto == "initialize") {
 ?>
 <h1>Database initialization</h1>
 <p>Clicking the button below, will intialize your database. This will erase all existing data and create new datase tables from scratch.</p>
-<form action="initbd.php" method="post">
-<button type="submit" name="toiminto" value="initialize" onclick="confirm('If you have any existing data, it will be erased. Continue?';)">Initialize</button>
+<form action="initdb.php" method="post">
+<button type="submit" name="toiminto" value="initialize" onclick="return confirm('If you have any existing data, it will be erased. Continue?')">Initialize</button>
 </form>
+<?php
+}
+?>
 </body>
 </html>
